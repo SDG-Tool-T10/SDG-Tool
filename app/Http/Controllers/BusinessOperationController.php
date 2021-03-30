ad27d6925605fe00864867725af28bc70ddde306
Hao Guo <guo0012@hz.nl>
2021-03-30 12:08:07 +0200

    created all model -a for creating blog

---
+++ app/Http/Controllers/BusinessOperationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BusinessOperation;
+use Illuminate\Http\Request;
+
+class BusinessOperationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\BusinessOperation  $businessOperation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BusinessOperation $businessOperation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BusinessOperation  $businessOperation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BusinessOperation $businessOperation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\BusinessOperation  $businessOperation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BusinessOperation $businessOperation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BusinessOperation  $businessOperation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BusinessOperation $businessOperation)
+    {
+        //
+    }
+}
