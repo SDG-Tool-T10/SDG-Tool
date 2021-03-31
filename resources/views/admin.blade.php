51ec56be4d56e3a9dd5294b1ff094ce13b801e43
AlexGit123 <70524253+AlexGit123@users.noreply.github.com>
2021-03-31 17:06:06 +0200

    developing admin panel

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,83 @@
+@extends('common.master')
+
+@section('content')
+
+    <div class="" style="padding: 10%">
+        <div class="columns">
+            <div class="column is-6 is-narrow">
+                <div class="card" style="width: 500px">
+                    <div class="card-content">
+                        <div class="content">
+                            <header class="card-header-title columns">Blogs
+
+                                <h1 style="padding-left: 50%">
+                                <button class="" style="">
+
+                                       <p>Add a Blog</p>
+
+                                </button>
+                                    </h1>
+                            </header>
+
+
+                            Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi
+                            porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes,
+                            nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="column is-6 is-narrow">
+                <div class="card" style="width: 500px">
+                    <div class="card-content">
+                        <div class="content">
+                            <header class="card-header-title columns">Blogs
+
+                                <h1 style="padding-left: 50%">
+                                    <button class="" style="">
+
+                                        <p>Add a Blog</p>
+
+                                    </button>
+                                </h1>
+                            </header>
+
+
+                            Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi
+                            porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes,
+                            nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="" style="padding-top: 10%">
+            <div class="column is-6 is-narrow">
+                <div class="card" style="width: 500px">
+                    <div class="card-content">
+                        <div class="content">
+                            <header class="card-header-title columns">Blogs
+
+                                <h1 style="padding-left: 50%">
+                                    <button class="" style="">
+
+                                        <p>Add a Blog</p>
+
+                                    </button>
+                                </h1>
+                            </header>
+
+
+                            Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi
+                            porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes,
+                            nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,5 @@ Route::get('/', function () {
 });
 
 Route::get('/blog', [BlogController::class, 'index']);
+
+Route::get('/admin', [AdminController::class, 'index']);
