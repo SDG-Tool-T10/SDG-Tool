a79b1f476c713cef32e104e10bfbcec8c4aea98c
AnnevanHeeren <70519503+AnnevanHeeren@users.noreply.github.com>
2021-04-22 14:11:28 +0200

    fixed requested changes

--- resources/views/common/master.blade.php
+++ resources/views/common/master.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>SDG Tool</title>
 
-    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    @yield('script')
+
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/bulma.min.css') }}" rel="stylesheet">
 </head>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,16 +1,20 @@
 @extends('common.master')
 
+@section('script')
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+@endsection
+
 @section('content')
-    <div class="columns" style="margin: 50px">
+    <div class="columns contact-columns">
         <div class="column is-12 has-text-centered"><h1 class="title is-1">Contact Us</h1></div>
     </div>
 
-    <section class="section" style="padding: 24px">
+    <section class="section contact-section">
         <div class="container">
             <div class="columns">
-                <div class="card" style="margin-left: 250px">
+                <div class="card contact-card">
                     <div class="card-content">
-                        <div class="content" style="margin: 50px">
+                        <div class="content contact-content">
 
                             <span class="icon-text">
                                 <span class="icon is-large mb-4">
@@ -18,27 +22,21 @@
                                 </span>
                                 <span class="is-size-4">Joeri Versluis</span>
                             </span>
-
                             <br>
-
                             <span class="icon-text">
                                 <span class="icon is-large mb-4">
                                     <i class="fas fa-2x fa-phone"></i>
                                 </span>
                                 <span class="is-size-4 is-spaced">06-123-456-78</span>
                             </span>
-
                             <br>
-
                             <span class="icon-text">
                                 <span class="icon is-large mb-6">
                                     <i class="fas fa-2x fa-at"></i>
                                 </span>
                                 <span class="is-size-4">joeriversluis@hz.nl</span>
                             </span>
-
                             <br>
-
                             <a href="https://www.linkedin.com/">
                             <span class="icon-text">
                                 <span class="icon is-large">
@@ -54,14 +52,12 @@
                 <section class="hero  is-medium  is-bold">
                     <div class="header-img columns">
                         <div class="column">
-                            <img style="height: 360px; width: 360px; margin: 10px 50px 50px 180px;" src="./image/contact.png" />
+                            <img class="contact-picture" src="./image/contact.png"/>
                         </div>
                     </div>
                 </section>
 
-
             </div>
         </div>
     </section>
-
 @endsection
