44dd89ac83c86b4f93c7e201746af84fd98d2dc0
Quinten <70522000+awqfrxx@users.noreply.github.com>
2021-04-23 12:05:58 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,15 +5,7 @@
         <x-auth-card>
             <x-slot name="logo">
             </x-slot>
-                {{--            <a href="/">--}}
-                {{--                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />--}}
-                {{--                <section class="hero  is-medium  is-bold">--}}
-                {{--                    <div class="header-img">--}}
-                {{--                        <img src="./image/sdg-header-en.png" />--}}
-                {{--                    </div>--}}
-                {{--                </section>--}}
-                {{--            </a>--}}
-
+                
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
