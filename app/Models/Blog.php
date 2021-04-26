2d3e9bc78319c1c1d874ea9395035e62ba1f7523
Koreyoshi159 <70514758+Koreyoshi159@users.noreply.github.com>
2021-04-26 09:52:54 +0200

    Changed subs_dgs to sub_sdgs

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -31,7 +31,7 @@ class Blog extends Model
         return $this->belongsToMany(Sdg::class);
     }
 
-    public function subs_dgs()
+    public function sub_sdgs()
     {
         return $this->belongsToMany(SubSdg::class);
     }
