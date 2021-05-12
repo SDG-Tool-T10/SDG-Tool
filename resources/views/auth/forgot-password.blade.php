8a7b9c294e881484881e8f09d6f6f436769b55ec
Koen Hamstra <koen@Koens-MacBook-Pro.local>
2021-05-12 14:03:33 +0200

    add forget password functionality

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,3 +1,6 @@
+@extends('common.master')
+<link href="{{ asset('css/tailwind.css') }}" rel="stylesheet">
+@section('content')
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
@@ -34,3 +37,4 @@
         </form>
     </x-auth-card>
 </x-guest-layout>
+@endsection
