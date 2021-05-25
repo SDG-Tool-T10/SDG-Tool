a8cce5a59d364731085e5d1f9cfbe2519dca31d2
AnnevanHeeren <70519503+AnnevanHeeren@users.noreply.github.com>
2021-05-25 13:48:05 +0200

    Added information blogs

--- resources/views/sdg.blade.php
+++ resources/views/sdg.blade.php
@@ -26,6 +26,7 @@
 
     <div class="columns columns-container is-multiline">
 
+        @foreach($sdg->blogs as $blog)
         <div class="column is-half">
             <div class="card">
                 <div class="card-image">
@@ -36,58 +37,22 @@
                 <div class="card-stacked">
                     <div class="card-content">
                         <div class="media-content">
-                            <p class="title is-4">Project 1/Blog</p>
+                            <p class="title is-4">{{ $blog->activity->name }} <i>Blog title</i></p>
                         </div>
                         <div class="subtitle is-6 sdg-content">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Phasellus nec iaculis mauris.
+                            Impact: {{ $blog->impact }}<br>
+                            Research Group: <br>
+                            Program: {{ $blog->program->name }}<br>
+                            Business Operation: {{ $blog->businessOperation->name }}<br>
+                            Subgoal: {{ $blog->subSdgs }}<br>
+                            Publisher: {{ $blog->contact_name }} <br>
+                            Updated at: {{ $blog->updated_at }} <br>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        <div class="column is-half">
-            <div class="card">
-                <div class="card-image">
-                    <figure class="image is-2by1">
-                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-                    </figure>
-                </div>
-                <div class="card-stacked">
-                    <div class="card-content">
-                        <div class="media-content">
-                            <p class="title is-4">Project 2/Blog</p>
-                        </div>
-                        <div class="subtitle is-6 sdg-content">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Phasellus nec iaculis mauris.
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="column is-half">
-            <div class="card">
-                <div class="card-image">
-                    <figure class="image is-2by1">
-                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-                    </figure>
-                </div>
-                <div class="card-stacked">
-                    <div class="card-content">
-                        <div class="media-content">
-                            <p class="title is-4">Project 3/Blog</p>
-                        </div>
-                        <div class="subtitle is-6 sdg-content">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Phasellus nec iaculis mauris.
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        @endforeach
 
     </div>
 @endsection
