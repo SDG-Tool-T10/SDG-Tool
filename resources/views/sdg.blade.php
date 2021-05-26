c94dccf81d5a1f1270ae9b722878d42c304ad6ce
Sanyi0001 <70522920+Sanyi0001@users.noreply.github.com>
2021-05-26 14:44:39 +0200

    conflict resolved

--- resources/views/sdg.blade.php
+++ resources/views/sdg.blade.php
@@ -31,15 +31,15 @@
                 <div class="card">
                     <div class="card-image">
                         <figure class="image is-2by1">
-                            <a href="//{{ $blog->link }}"><img src="https://bulma.io/images/placeholders/1280x960.png"
-                                                               alt="Placeholder image"></a>
+                            <a href="//{{ $blog->link }}">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"></a>
                         </figure>
                     </div>
                     <div class="card-stacked">
                         <div class="card-content">
                             <div class="media-content">
-                                <a class="title is-4" href="//{{ $blog->link }}">{{ $blog->activity->name }}<i> Blog
-                                        title</i></a>
+                                <a class="title is-4" href="//{{ $blog->link }}">{{ $blog->activity->name }}
+                                    <i> Blog title</i></a>
                             </div>
                             <div class="subtitle is-6 sdg-content">
                                 Impact: {{ $blog->impact }}<br>
