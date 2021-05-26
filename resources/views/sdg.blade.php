ff730bf2ff3ab4a5af846a530523c30dfe5046a8
Sanyi0001 <70522920+Sanyi0001@users.noreply.github.com>
2021-05-26 14:43:16 +0200

    conflict resolved

--- resources/views/sdg.blade.php
+++ resources/views/sdg.blade.php
@@ -25,9 +25,7 @@
     </div>
 
     <div class="columns columns-container is-multiline">
-
         @foreach($sdg->blogs as $blog)
-
             @continue($blog->visibility == 0)
             <div class="column is-half">
                 <div class="card">
@@ -51,12 +49,11 @@
                                 Subgoal: {{ $blog->subSdgs }}<br>
                                 Publisher: {{ $blog->contact_name }} <br>
                                 Updated at: {{ $blog->updated_at }} <br>
-
                             </div>
                         </div>
                     </div>
                 </div>
-                @endforeach
-
             </div>
+        @endforeach
+    </div>
 @endsection
