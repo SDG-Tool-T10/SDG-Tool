61fc5dbf9c1a949a74ab6a2f4b945a0af1f125b7
Quinten <70522000+awqfrxx@users.noreply.github.com>
2021-06-01 11:15:20 +0200

    added factory that creates 50 fake blogs

--- database/seeders/BlogSeeder.php
+++ database/seeders/BlogSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Blog;
+use App\Models\Sdg;
 use Illuminate\Database\Seeder;
 
 class BlogSeeder extends Seeder
@@ -15,5 +16,13 @@ class BlogSeeder extends Seeder
     public function run()
     {
         Blog::factory()->count(50)->create();
+
+        $sdgs = Sdg::all();
+
+        Blog::all()->each(function ($blog) use ($sdgs) {
+            $blog->sdgs()->attach(
+                $sdgs->random(1)->pluck('id')->toArray()
+            );
+        });
     }
 }
