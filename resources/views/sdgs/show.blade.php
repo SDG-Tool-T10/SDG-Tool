a037d6fff1ebe4f29f238a324d7f53126e4e949b
Quinten <70522000+awqfrxx@users.noreply.github.com>
2021-06-15 14:37:41 +0200

    blog design on sdg.show

--- resources/views/sdgs/show.blade.php
+++ resources/views/sdgs/show.blade.php
@@ -22,39 +22,62 @@
             </div>
         </div>
     </div>
-
-    <div class="columns columns-container is-multiline">
-        @foreach($sdg->blogs as $blog)
-            @continue($blog->visibility == 0)
-            <div class="column is-half">
-                <div class="card">
+    
+    <div class="columns columns-container is-multiline blogs">
+        @forelse($sdg->blogs as $blog)
+            @continue(!$blog->visibility)
+            <div class="column is-full blogPost">
+                <div class="card card_with_blog">
                     <div class="card-stacked">
-                        <div class="card-content">
-                            <div class="media-content">
-                                <a class="title is-4" href="//{{ $blog->link }}">
-                                    {{ $blog->activity->name }} - {{ $blog->title }}
+                        <div class="card-content card_container">
+                            <div class="media-content card_link">
+                                <a class="title blogTitle is-3" href="//{{ $blog->link }}">
+                                    {{ $blog->title }}
                                 </a>
                             </div>
                             <div class="subtitle is-6 sdg-content">
-                                Impact: {{ $blog->impact }}<br>
-                                Research Group: {{ $blog->research_group->name ?? 'empty' }}<br>
-                                Program: {{ $blog->program->name ?? 'empty' }}<br>
-                                Business Operation: {{ $blog->business_operation->name ?? 'empty' }}<br>
-                                SDGs:
-                                @foreach($blog->sdgs as $sdg)
-                                    {{ $sdg->name }}
-                                @endforeach<br>
-                                Subgoal:
-                                @foreach($blog->sub_sdgs as $sub_sdg)
-                                    {{ $sub_sdg }}
-                                @endforeach<br>
-                                Publisher: {{ $blog->contact_name }} <br>
-                                Updated at: {{ $blog->updated_at }} <br>
+                                <p class="card_title">Impact:</p>
+                                <p class="card_content-full">{{ $blog->impact }}</p>
+                                <div>
+                                    @foreach($blog->sdgs as $sdg)
+                                        <p class="card_title">SDG:</p>
+                                        <p class="card_content-full blogSDG">{{ $sdg->name }}</p>
+                                    @endforeach
+                                </div>
+                                <div class="blog_container">
+                                    <p class="card_title">Activity:</p>
+                                    <p class="card_content-half activity">{{ $blog->activity->name }}</p>
+                                </div>
+                                <div class="blog_container">
+                                    <p class="card_title">Research Group:</p>
+                                    <p class="card_content-half researchGroup">{{ $blog->research_group->name ?? 'empty' }}</p>
+                                </div>
+                                <br>
+                                <div class="blog_container">
+                                    <p class="card_title">Program:</p>
+                                    <p class="card_content-half program">{{ $blog->program->name ?? 'empty' }}</p>
+                                </div>
+                                <div class="blog_container">
+                                    <p class="card_title">Business Operation:</p>
+                                    <p class="card_content-half businessOperation">{{ $blog->business_operation->name ?? 'empty' }}</p>
+                                </div>
+                                <div class="blog_container">
+                                    @foreach($blog->sub_sdgs as $sub_sdg)
+                                        <p class="card_title">SDG:</p>
+                                        <p>{{ $sub_sdg->name }}</p>
+                                    @endforeach
+                                </div>
+                                <div class="blog_container-bottom">
+                                    <p class="blog_footer"><b>Publisher:</b> {{ $blog->contact_name }} </p>
+                                    <p class="blog_footer"><b>Updated at:</b> {{ $blog->updated_at }} </p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        @endforeach
+        @empty
+            <p>No blogs to show at the moment.</p>
+        @endforelse
     </div>
 @endsection
